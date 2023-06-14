007f73fc622655ae1c31ef1c04c5c8a7b8896862
Marc von Weissenfluh <m.vw@gmx.ch>
2023-06-14 09:28:29 +0200

    add main

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+* @file main.c
+* @date 14.06.2023
+* @author Marc von Weissenfluh
+* 
+* @brief This is the main file
+*/
+
+void main(void)
+{
+    // do some initialization
+    while(1)
+    {
+        // do some stuff
+    }
+}
