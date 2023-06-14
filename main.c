76b7bc357b5ff71be4bd2a6e96faae0882da99b5
Marc von Weissenfluh <m.vw@gmx.ch>
2023-06-14 10:41:46 +0200

    add comment for merge conflict

--- main.c
+++ main.c
@@ -10,6 +10,9 @@ void main(void)
 {
     // call my ne function
     myCoolFun(0);
+
+    // this is a new comment for merge conflict example by cool feature branch
+
     while(1)
     {
         // do some stuff
